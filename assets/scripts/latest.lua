40cd298bd5ad20ab5cb9903d082bfd9d059c5e73
aiden5070 <46075799+aiden5070@users.noreply.github.com>
2019-06-11 19:51:06 -0400

    Update latest.lua

--- assets/scripts/latest.lua
+++ assets/scripts/latest.lua
@@ -2,23 +2,26 @@ if syn then
   game:service('Players').LocalPlayer:Kick("fat synapse user")
 end
 
-local gui_name = ""
-function generate_name()
-  local a = {}
-  local aa = ""
-  for i = 1, 30 do
-    local b = string.char(math.random(98,122))
-    if math.random(1,2)then b=b:upper()end
-    table.insert(a,b)
-  end
-  local c = 1
-  for i,v in pairs(a)do
-    c = c + 1
-    if c > 4 then
-      aa = aa.."-"
-    end
-    aa = aa.. v
-  end
-  return aa
+_G.gui_name = ""
+function GenerateKey()
+	local a = {}
+	for i = 1, 30 do
+		local char = string.char(math.random(98,122))
+		if math.random(1,2)==2 then
+			char = char:upper()
+		end
+		table.insert(a,char)
+	end
+	local b = ""
+	local c = 1
+	for i,v in pairs(a) do
+		c = c + 1
+		if c > 4 then
+			c = 0
+			b = b.."-"
+		end
+		b = b.. v
+	end
+	return b
 end
-gui_name = generate_name()
+_G.gui_name = GenerateKey()
